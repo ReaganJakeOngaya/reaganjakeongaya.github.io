57b1e380fcc0167278c79cc1b699103c6595e68c
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-01-20 12:54:32 +0300

    Navbar, Home, about settup done

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// Navbar
 const mobileMenu = document.getElementById("mobile-menu");
 const navLinks = document.querySelector(".nav-links");
 
 mobileMenu.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// Home 
+const professions = ["A Software Developer", "A Problem Solver", "A Tech Enthusiast"];
+let i = 0;
+
+function typeText() {
+    const dynamicText = document.querySelector(".dynamic-text");
+    dynamicText.textContent = professions[i];
+    i = (i + 1) % professions.length;
+}
+
+setInterval(typeText, 2000);
